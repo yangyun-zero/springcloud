b4921e03ac7d6b4b043ff75bc277825dd64b902c
413 <yy502119341@163.com>
2020-04-23 17:27:58 +0800

    测试分支

--- microservice-config-client-3355/src/main/java/com/yangyun/springcloud/ConfigClient_3355_StartSpringCloudApp.java
+++ microservice-config-client-3355/src/main/java/com/yangyun/springcloud/ConfigClient_3355_StartSpringCloudApp.java
@@ -10,11 +10,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date 2019/9/12 0012 15:46
  * @Version 1.0
  **/
-@SpringBootApplication
+//@SpringBootApplication
 public class ConfigClient_3355_StartSpringCloudApp {
 
     public static void main(String[] args) {
-        System.out.println("");
-        SpringApplication.run(ConfigClient_3355_StartSpringCloudApp.class, args);
+//        System.out.println("");
+//        SpringApplication.run(ConfigClient_3355_StartSpringCloudApp.class, args);
+        System.out.println(Integer.compare(3,3) > 0);
     }
+
+
 }
